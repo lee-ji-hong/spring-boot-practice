9ef32789733b074c11bd3b679afbae81afe95749
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2025-08-10 22:19:11 +0900

    Feat: "CORS 방지"

---
+++ prac/src/main/java/restapi/prac/config/WebConfig.java
@@ -0,0 +1,22 @@
+package restapi.prac.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // 모든 경로에 대해 적용
+                        .allowedOrigins("http://localhost:5173")  // 허용할 출처 지정
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
